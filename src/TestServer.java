97a901803d8396914e4a3b1df992d0444229e44f
irina08 <irina_ger@yahoo.com>
2020-10-06 11:52:09 -0700

    added to TestServer

---
+++ src/TestServer.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class TestServer {
+    public static void main(String[] args) {
+        HashMap servers = new HashMap<Character, Integer>();
+        servers.put('X', 3);
+        servers.put('Y', 1);
+        servers.put('Z', 1);
+
+        LoadBalancer balancer = new LoadBalancer(servers);
+
+    }
+}
