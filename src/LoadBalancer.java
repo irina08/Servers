0b232ae837a638beef3ae8dfa01c5b4e4e92e3a8
irina08 <irina_ger@yahoo.com>
2020-10-07 14:51:00 -0700

    added to TestServer

--- src/LoadBalancer.java
+++ src/LoadBalancer.java
@@ -3,9 +3,18 @@ import java.util.*;
 public class LoadBalancer {
     private Map<Character, Integer> map;
     private Set<Character> keys;
+    private int range = 0;
 
     public LoadBalancer(HashMap<Character, Integer> servers) {
         this.map = servers;
         keys = map.keySet();
+    }
+
+    public  void convertValueToRanges() {
+        for(Character key: keys) {
+            range = range + map.get(key);
+            map.put(key, range);
+           System.out.println("range: " + range);
+        }
     }
 }
